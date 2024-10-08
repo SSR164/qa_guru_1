4167b141cbaa7f5ccabfc151b300d41af32222ef
Serg Sh. <vampir6307@gmail.com>
2024-10-08 22:28:40 +0300

    зменения в коде

--- src/main/java/com/vampir6307/Main.java
+++ src/main/java/com/vampir6307/Main.java
@@ -2,7 +2,7 @@ package com.vampir6307;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello qa_guru66666!");
 
 
     }
